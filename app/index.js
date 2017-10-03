94de8f8030895dd44a82228e6851dda3262d0933
Summer Saleh <ssaleh@netjets.com>
2017-10-03 15:32:34 -0400

    some js code

--- app/index.js
+++ app/index.js
@@ -1,1 +1,41 @@
-console.log('Hello World!');
+let a = 'hello';
+
+console.log(a);
+
+{
+	let a = 'goodbye';
+	console.log(a);
+}
+
+const array = [1, 2, 3];
+array.push(4);
+console.log(array);
+
+let d = `hello ${a}`;
+console.log(d);
+
+let spread = [7, 8, 9];
+let spread2 = [6, ...spread, 10];
+console.log(spread2);
+
+function print (a, b, c) {
+	console.log(a, b, c);
+}
+
+let z = [1, 2, 5];
+print(...z);
+
+//can add as many args as you want to this function
+function print2 (...s) {
+	console.log(s);
+}
+
+function butter(...a) {
+  console.log(a);
+}
+
+let b = [1, 2, 3];
+butter(...b, 4, 5, 6);
+
+
+print2(1, 2, 3);
